238065333753a018b7eb709f33a91103b5749407
Pascal <pr.pascal.bachmann@gmail.com>
2025-02-09 19:40:26 +0100

    cleanup proxy

--- src/routes/api/test/+server.ts
+++
@@ -1,7 +0,0 @@
-import { json } from '@sveltejs/kit';
-
-export async function GET() {
-	console.log('🔍 GET request received at /api/my-endpoint');
-
-	return json({ message: 'Test: API is working!' });
-}
